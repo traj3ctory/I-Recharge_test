eb8d026ecab4d0a2613b8724a67dca5946512e37
traj3ctory <davidbenard.bd@gmail.com>
2022-01-19 07:45:01 +0100

    feat: :art: add the images for each product
    add the script and style to control the active class

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,10 +1,30 @@
+/**
+ * Function to switch image based on button-color click
+ * @param {string} dataColor
+ * @returns {string} color
+ */
 const productImg = document.getElementById("product_img");
 const button = document.querySelectorAll("button.btn_color");
 
-button.forEach((el) => {
+// Loop through the array of buttons with className btn_color
+button.forEach((el, i) => {
+  // Add event listener {click} to each button
   el.addEventListener("click", (e) => {
+    const index = el.getAttribute("data-item");
     e.preventDefault();
+    // Get the data-color attribute from the button
     const color = e.target.getAttribute("data-color");
+    // Change the image src to the data-color attribute
     productImg.setAttribute("src", `img/iphone_${color}.jpeg`);
+    // Remove class active from other button onClick of a button
+    removeSelected(button);
+    // Add a class active onClick of a button
+    el.classList.add("active");
   });
 });
+
+const removeSelected = (items) => {
+  for (let i = 0; i < items.length; i++) {
+    items[i].classList.remove("active");
+  }
+};
